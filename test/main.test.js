b190afd715fc08e6e2d2ba6da7f502cc076e0341
bomma.anil <anil.bommaa@gmail.com>
2019-09-30 11:49:07 -0500

    updated test case

--- test/main.test.js
+++ test/main.test.js
@@ -8,32 +8,13 @@ QUnit.test('TEST add', assert => {
 
 QUnit.config.autostart = false  // sync = false; start after loading html
 
-window.addEventListener('load', () => {
-  const appURL = '../index.html'
-  const openingTag = '<main class="container mt-5 flex-fill">'
-  const closingTag = '</main>'
-  fetch(appURL, { method: 'GET' })
-    .then(response => {
-      return response.text() // returns promise
-    })
-    .then(txt => {
-      const start = txt.indexOf(openingTag)
-      const end = txt.indexOf(closingTag) + closingTag.length
-      const html = txt.substring(start, end)
-      const qunitFixtureBody = document.getElementById('qunit-fixture')
-      qunitFixtureBody.innerHTML = html
-      console.info(qunitFixtureBody)
-      QUnit.start()
-    })
-    .catch(error => {
-      console.error('error:', error);
-      QUnit.start()
-    })
-})
+
+QUnit.start();
 
 QUnit.test("TEST maxLength number validation", assert => {
   const input = document.querySelector('#maxLength')
   const warning = document.querySelector('#firstWarning')
+  console.log(input);
   input.value = -3;
   assert.equal(input.value, -3, "Bad value assigned")
   assert.strictEqual(input.checkValidity(), false, "Correctly fails validation")
